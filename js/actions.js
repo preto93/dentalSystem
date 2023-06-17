c1251644fe667b2ce42685fb9093ddc1c802689b
preto93 <federicotartari93@gmail.com>
2023-06-17 20:01:53 -0300

    Agregando acciones de usuario en JS

--- js/actions.js
+++ js/actions.js
@@ -1,3 +1,57 @@
+// Acciones de Login
+document.getElementById("newuser").addEventListener("click", function(event) {
+    // Evita el comportamiento predeterminado del enlace
+    event.preventDefault();
+    
+    // Pagina a redirecccionar al usuario
+    window.location.href = "newuser.html";
+  });
+  
+  document.getElementById("ingreso").addEventListener("click", function(event) {
+    // Evita el comportamiento predeterminado del enlace
+    event.preventDefault();
+    
+
+    // Enviando al usuario al HOME
+    window.location.href = "home.html";
+    console.log("Bienvenido @Usuario");
+  });
+
+  //Acciones dentro de nuevo usuario
+  document.getElementById("registro").addEventListener("click", function(event) {
+    // Evita el comportamiento predeterminado del enlace
+    event.preventDefault();
+    
+    // Pagina a redirecccionar al usuario > Home
+    window.location.href = "home.html";
+    console.log("Bienvenido @User, gracias por registrarse");
+  });
+
+  document.getElementById("backinicio").addEventListener("click", function(event) {
+    // Evita el comportamiento predeterminado del enlace
+    event.preventDefault();
+    
+    // Pagina a redirecccionar al usuario > INICIO
+    window.location.href = "inicio.html";
+    console.log("Regresando al inicio");
+  });
+
+
+
+  //
+
+  document.getElementById("registro").addEventListener("click", function(event) {
+    // Evita el comportamiento predeterminado del enlace
+    event.preventDefault();
+    
+    // Pagina a redirecccionar al usuario > Home
+    window.location.href = "home.html";
+    console.log("Redireccionando al home");
+  });
+  
+
+  // Acciones de tablas > odontologos/pacientes, agregar, eliminar, editar
+
 function agregarElemento() {
     var div = document.getElementById('actions');
     var nuevoElemento = document.createElement('p');
@@ -7,7 +61,7 @@ function agregarElemento() {
 
 // Función para eliminar un elemento
 function eliminarElemento() {
-    var elementoEliminar = document.getElementById('elementoModificar');
+    var elementoEliminar = document.getElementById('elementoEliminar');
     var padre = elementoEliminar.parentNode;
     padre.removeChild(elementoEliminar);
 }
@@ -17,3 +71,4 @@ function modificarElemento() {
     var elementoModificar = document.getElementById('elementoModificar');
     elementoModificar.innerHTML = 'Elemento modificado';
 }
+
