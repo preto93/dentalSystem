02093aa4146ca83cf9ac486d8c5b69e849558e5e
pretojed <federicotartari93@gmail.com>
2023-06-21 22:35:17 -0300

    Dando estilos a las tablas / forms

--- js/actions.js
+++ js/actions.js
@@ -4,26 +4,28 @@ document.getElementById("newuser").addEventListener("click", function(event) {
     event.preventDefault();
     
     // Pagina a redirecccionar al usuario
-    window.location.href = "newuser.html";
+    window.location.href = "#nuevousuario";
   });
   
-  document.getElementById("ingreso").addEventListener("click", function(event) {
+  document.getElementById("ingresar").addEventListener("click", function(event) {
     // Evita el comportamiento predeterminado del enlace
     event.preventDefault();
     
 
     // Enviando al usuario al HOME
-    window.location.href = "home.html";
+    window.location.href = "#home";
     console.log("Bienvenido @Usuario");
   });
 
-  //Acciones dentro de nuevo usuario
-  document.getElementById("registro").addEventListener("click", function(event) {
+
+
+  //>>>>>Acciones dentro de nuevo usuario><<<
+  document.getElementById("registrarse").addEventListener("click", function(event) {
     // Evita el comportamiento predeterminado del enlace
     event.preventDefault();
     
     // Pagina a redirecccionar al usuario > Home
-    window.location.href = "home.html";
+    window.location.href = "#home";
     console.log("Bienvenido @User, gracias por registrarse");
   });
 
@@ -32,27 +34,13 @@ document.getElementById("newuser").addEventListener("click", function(event) {
     event.preventDefault();
     
     // Pagina a redirecccionar al usuario > INICIO
-    window.location.href = "inicio.html";
-    console.log("Regresando al inicio");
+    window.location.href = "#inicio";
+    alert('Redireccionando al inicio');
   });
 
-
-
-  //
-
-  document.getElementById("registro").addEventListener("click", function(event) {
-    // Evita el comportamiento predeterminado del enlace
-    event.preventDefault();
-    
-    // Pagina a redirecccionar al usuario > Home
-    window.location.href = "home.html";
-    console.log("Redireccionando al home");
-  });
-  
 
   // Acciones de tablas > odontologos/pacientes, agregar, eliminar, editar
-
-function agregarElemento() {
+  function modificarElemento()
     var div = document.getElementById('actions');
     var nuevoElemento = document.createElement('p');
     nuevoElemento.innerHTML = 'Nuevo elemento agregado';
@@ -64,6 +52,7 @@ function eliminarElemento() {
     var elementoEliminar = document.getElementById('elementoEliminar');
     var padre = elementoEliminar.parentNode;
     padre.removeChild(elementoEliminar);
+    alert('Elemento eliminado correctamente');
 }
 
 // Función para modificar un elemento
@@ -72,3 +61,17 @@ function modificarElemento() {
     elementoModificar.innerHTML = 'Elemento modificado';
 }
 
+// Desplegable Menu
+
+const toggleCheckbox = document.getElementById('toggleCheckbox');
+
+toggleCheckbox.addEventListener('change', function() {
+  // No se necesita código JavaScript adicional en este caso
+});
+
+// Boton Confirmar turno
+const okturnos = document.getElementById('okturno');
+
+okturnos.addEventListener('click', function(event) {
+  alert('El turno se ha reservado con éxito');
+});
