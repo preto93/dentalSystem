f577417f304485478c2b447126aa1cfea67483b2
pretojed <federicotartari93@gmail.com>
2023-07-01 22:47:43 -0300

    Turnos: Agregando listas desplegables y guardando acciones en JS

--- js/turno.js
+++ js/turno.js
@@ -1,16 +1,39 @@
 
-// Boton Confirmar turno
 const okturnos = document.getElementById('okturno');
 
 okturnos.addEventListener('click', function(event) {
+  const pacienteSeleccionado = document.getElementById('listadopacientes').value;
+  const odontologoSeleccionado = document.getElementById('listadoOdontologos').value;
+  const fechaSeleccionada = document.getElementById('fechasDisponibles').value;
+
   alert('El turno se ha reservado con éxito');
+  guardarAccion(pacienteSeleccionado, odontologoSeleccionado, fechaSeleccionada);
 });
 
+function guardarAccion(paciente, odontologo, fecha) {
+  const body = {
+    paciente: paciente,
+    odontologo: odontologo,
+    fecha: fecha
+  };
 
-function vaciarFormulario() {
-  document.getElementById("username2").value = "";
-  document.getElementById("password2").value = "";
-  document.getElementById("roles").selectedIndex = -1;
+  console.log(body);
+
+  const url = "https://dentalsystem-production.up.railway.app/api/v1/turno/registrar";
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  };
+
+  fetch(url, options)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
-
-
