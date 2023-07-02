f718680f1dada5d895b42c36fee6a1daa88d077e
pretojed <federicotartari93@gmail.com>
2023-07-01 23:34:29 -0300

    Modificando js

--- js/registro.js
+++ js/registro.js
@@ -1,74 +1,4 @@
-// Desplegable Menu ROL
 
-const toggleCheckbox = document.getElementById('toggleCheckbox');
-
-toggleCheckbox.addEventListener('change', function() {
-});
-
-// Seleccionar Rol
-function expandirSeleccion(checkbox) {
-  var seleccion = document.getElementById("seleccion");
-  
-  if (checkbox.checked) {
-    seleccion.removeAttribute("disabled");
-    seleccion.classList.add("expandido");
-  } else {
-    seleccion.setAttribute("disabled", "disabled");
-    seleccion.classList.remove("expandido");
-  }
-}
-
-/*//>>>>>Acciones dentro de nuevo usuario><<<
-document.getElementById("registrarse").addEventListener("click", function(event) {
-    // Evita el comportamiento predeterminado del enlace
-    event.preventDefault();
-    
-    guardarAccion ();
-
-    // Pagina a redirecccionar al usuario > Home
-    window.location.href = "/home.html";
-    console.log("Bienvenido @User, gracias por registrarse");
-    alert("Bienvenido @User, gracias por registrarse");
-  });
-
-
-  document.getElementById("backinicio").addEventListener("click", function(event) {
-    // Evita el comportamiento predeterminado del enlace
-    event.preventDefault();
-    
-    // Pagina a redirecccionar al usuario > INICIO
-    window.location.href = "#inicio";
-    alert('Redireccionando al inicio');
-  });
-
-
-  // Guardando nuevo usuario 
-function guardarAccion() {
-    var usuario = document.getElementById("username2").value;
-    var pw = document.getElementById("password2").value;
-    var rol = Array.from(document.getElementById("roles").selectedOptions).map(option => option.value);
-    
-    const body = {
-        username : usuario, 
-        password : pw, 
-        rol : rol [0]
-    }
-
-
-    const url = "http://localhost:8086/api/v1/usuario/registrar";
-    const options = {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(body)
-  };
-
-
-
-
-};
-*/
 
 const form = document.querySelector('form');   
 
@@ -143,12 +73,4 @@ function realizarRegister(datos) {
       console.error(error);
       alert('Upss tenemos un error :(');
   })
-<<<<<<< HEAD
-    .catch(error => {
-    console.error(error); 
-  });
 }
-
-=======
-};
->>>>>>> beab56e306b7414ebe384a07d1ac91006fb6adea
