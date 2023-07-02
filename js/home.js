c03644e1600a7163a597cb554d51774c5732a866
pretojed <federicotartari93@gmail.com>
2023-07-01 22:54:08 -0300

    Funcionalidad cerrar sesión

--- js/home.js
+++ js/home.js
@@ -26,3 +26,24 @@ document.getElementById("buttonturno").addEventListener("click", function(event)
     window.location.href = "/paciente.html";
   });
   
+  // Cerrar Sesion
+const salirButton = document.getElementById("cerrarsesion");
+
+// Agregar el evento de clic al botón "Salir"
+salirButton.addEventListener("click", function(event) {
+  event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+
+  // Lógica para cerrar sesión
+  cerrarSesion();
+  
+  // Redireccionar al usuario a la página de inicio
+  window.location.href = "index.html";
+});
+
+// Función para cerrar sesión
+function cerrarSesion() {
+  // Realizar las acciones necesarias para cerrar la sesión
+
+  // Mostrar una alerta de cierre de sesión
+  alert("Sesión cerrada exitosamente");
+}
